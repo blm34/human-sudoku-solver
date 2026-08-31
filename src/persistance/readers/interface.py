66c42d085bc28a41232c3f09bcff8d9044616219
blm34 <48328783+blm34@users.noreply.github.com>
2026-08-31 15:57:11 +0100

    feat: add interfaces for reading and writing sudokus from a text stream

---
+++ src/persistance/readers/interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import TextIO
+    from grid import GridState
+
+
+class AbsSudokuReader(ABC):
+    @abstractmethod
+    def read(self, stream: TextIO) -> GridState:
+        """Read a sukoku from a text stream."""
+        raise NotImplementedError
