89684bae66526ec5341667f07b0e40e265d383ef
blm34 <48328783+blm34@users.noreply.github.com>
2026-08-31 22:47:11 +0100

    feat: add the concrete strategies to the strategy __init__.py

---
+++ src/strategy/__init__.py
@@ -0,0 +1,10 @@
+from .deduction import DigitDeduction, EliminationDeduction
+from .hidden_single import HiddenSingleStrategy
+from .naked_single import NakedSingleStrategy
+
+__all__ = [
+    "DigitDeduction",
+    "EliminationDeduction",
+    "HiddenSingleStrategy",
+    "NakedSingleStrategy",
+]
